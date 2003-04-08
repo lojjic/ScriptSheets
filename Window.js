b99c291f07a7e9a30d4f4f75e5c63043925cb2e4
Jason Johnston <github@lojjic.net>
2003-04-08 03:13:53 +0000

    add documentation create getWidth, getMouseX... methods with more detailed logic probably more

--- Window.js
+++ Window.js
@@ -1,47 +1,72 @@
-/*	<style type="text/css">
+/*===============
+
+Window.js: Creates a draggable, resizable "window" from any node.
+
+==Usage:==
+new Window(contentElement, titleElement, width, height, xPosition, yPosition, zIndex);
+	contentElement = the element to be encapsulated in a Window. Can be an element 
+		already existing within the document or script-generated (document.createElement).
+		This element is given the CSS class .window-content
+	titleElement = the node to be put in the title bar of the Window. Can be a node
+		already existing within the document (such as an <h1/> before or within the 
+		contentElement) or script-generated. If it exists within the document it will
+		be moved from its original location in the node tree to the title bar.
+		The title bar element is given the CSS class .window-title-bar
+	width = initial width of the Window in pixels. If not supplied, Window.minWidth 
+		below will be used instead.
+	height = initial height of the Window in pixels. If not supplied, Window.minHeight
+		below will be used instead.
+	xPosition = initial horizontal position in pixels. Defaults to zero if not supplied.
+	yPosition = initial vertical position in pixels. Defaults to zero if not supplied.
+	zIndex = initial z-index of the Window, for when windows overlap. Not very useful.
+	
+==Example Styles:==
+<style type="text/css">
 .window {position:absolute; border:2px outset #FFF; background:#EEE;}
 .window-content {border:2px inset #FFF; overflow:auto; padding-left:4px;}
 .window-title-bar {margin:0; padding:.25em 1em; margin:0; background:#009; color:#FFF; font-weight:bold; cursor:default; white-space:nowrap; -moz-user-select:none;}
 .window-resizer {position:absolute; right:-5px; bottom:-5px; width:16px; height:16px; border:solid #666; border-width:0 1px 1px 0; cursor:se-resize;}
-</style> */
+</style>
+
+===============*/
 
 
 
-function Window(elt, title, w, h, x, y, z) {
+function Window(contentElt, titleElt, w, h, x, y, z) {
 	// Freaks out Mac IE: // if(!(this instanceof Window)) return new Window(elt, title);
 	this.windowW = w;
 	this.windowH = h;
 	this.windowX = x;
 	this.windowY = y;
 	this.windowZ = z;
-	this.init(elt, title);
+	this.init(contentElt, titleElt);
 }
+Window.minWidth  = 100;
+Window.minHeight = 100;
 Window.topZIndex = 0;
 Window.instances = [];
 
 Window.prototype = {
-	init : function(contentElt, title) {
+	init : function(contentElt, titleElt) {
 		var elt = this.element = document.createElement("div");
-		var thisRef = this;
-		
+		var thisRef = this;		
 
 		//Make it positioned:
 		elt.className = "window";
 		elt.style.position = "absolute";
-		if (!elt.style.left) elt.style.left = (this.windowX || 0) + "px";
-		if (!elt.style.top)  elt.style.top  = (this.windowY || 0) + "px";
-		if (!elt.style.width)  elt.style.width  = (this.windowW || 500) + "px";
-		if (!elt.style.height) elt.style.height = (this.windowH || 500) + "px";
+		elt.style.left = (this.windowX || 0) + "px";
+		elt.style.top  = (this.windowY || 0) + "px";
+		elt.style.width  = (this.windowW || Window.minWidth ) + "px";
+		elt.style.height = (this.windowH || Window.minHeight) + "px";
 
 		//Make it pop to front when clicked:
 		if(this.windowZ > Window.topZIndex) Window.topZIndex = this.windowZ;
 		elt.addEventListener("mousedown", function(){thisRef.bringToFront();}, false);
-		
-		
+				
 		//Create title bar:
 		var titlebar = document.createElement("div");
 			titlebar.className = "window-title-bar";
-			titlebar.appendChild(document.createTextNode(title));
+			titlebar.appendChild(titleElt);
 			titlebar.addEventListener("mousedown", function(evt){thisRef.startDrag(evt);}, false);
 			titlebar.onselectstart = titlebar.ondragstart = function(){return false;} //prevent text selection
 		elt.appendChild(titlebar);
@@ -62,14 +87,13 @@ Window.prototype = {
 			position = "absolute";
 			left = right = bottom = "0"; top = "20px";
 		}
-			//Hack for IE:
+			//Hack to make height adjust to container in IE:
 			if(contentElt.style.setExpression) contentElt.style.setExpression("height", "parseFloat(" + elt.uniqueID + ".style.height) - 20");
 		this.element.appendChild(contentElt);
 		(document.getElementsByTagName("body")[0] || document.documentElement).appendChild(this.element);
-	},
-	
-	getStyle : function(prop) {
-		return window.getComputedStyle(this.element, null).getPropertyValue(prop);
+		
+		//Make sure it's not overlapping:
+		this.moveToOpenSpace();
 	},
 	
 	bringToFront : function() {
@@ -77,26 +101,26 @@ Window.prototype = {
 	},
 	
 	moveToOpenSpace : function() {
-		var elt = this.element;
+		var thisElt = this.element;
 		var wins = Window.instances;
 		var isOverlapping = false;
 		for(var i=0; i<wins.length; i++) {
 			if(wins[i] != this) {
-				var winElt = wins[i].element;
-				var xA = parseFloat(elt.style.left);
-				var yA = parseFloat(elt.style.top);
-				var wA = elt.offsetWidth;
-				var hA = elt.offsetHeight;
-				var xB = parseFloat(winElt.style.left);
-				var yB = parseFloat(winElt.style.top);
-				var wB = winElt.offsetWidth;
-				var hB = winElt.offsetHeight;
+				var otherElt = wins[i].element;
+				var xA = this.getLength("left");
+				var yA = this.getLength("top");
+				var wA = this.getWidth();
+				var hA = this.getHeight();
+				var xB = parseFloat(otherElt.style.left);
+				var yB = parseFloat(otherElt.style.top);
+				var wB = otherElt.offsetWidth;
+				var hB = otherElt.offsetHeight;
 
 				if((xA <= xB && xA + wA > xB && (yA <= yB && yA + hA > yB || yB <= yA && yB + hB > yA)) ||
 				   (xB <= xA && xB + wB > xA && (yA <= yB && yA + hA > yB || yB <= yA && yB + hB > yA))) {
 					//move to closer side:
-					if(xB + wB - xA < yB + hB - yA) elt.style.left = (this.windowX = xB + wB + 4) + "px";
-					else elt.style.top  = (this.windowY = yB + hB + 4) + "px";
+					if(xB + wB - xA < yB + hB - yA) thisElt.style.left = (this.windowX = xB + wB + 4) + "px";
+					else thisElt.style.top  = (this.windowY = yB + hB + 4) + "px";
 					isOverlapping = true;
 				}  
 			}
@@ -108,12 +132,10 @@ Window.prototype = {
 		var elt = this.element;
 
 		//Capture initial positions to compare to later:
-		this.mouseX = evt.clientX + (window.scrollX || document.body.scrollLeft);
+		this.mouseX = this.getMouseX(evt);
 		this.windowX = parseFloat(elt.style.left);
-		this.mouseY = evt.clientY + (window.scrollY || document.body.scrollTop);
+		this.mouseY = this.getMouseY(evt);
 		this.windowY = parseFloat(elt.style.top);
-
-		//document.body.className += " panel-dragging"; //hides controls while dragging
 
 		this.stopDrag();
 		var thisRef = this;
@@ -125,8 +147,8 @@ Window.prototype = {
 		var elt = this.element;
 
 		//Calc distance mouse moved:
-		var diffX = -this.mouseX + (this.mouseX = evt.clientX + (window.scrollX || document.body.scrollLeft));
-		var diffY = -this.mouseY + (this.mouseY = evt.clientY + (window.scrollY || document.body.scrollTop));
+		var diffX = -this.mouseX + (this.mouseX = this.getMouseX(evt));
+		var diffY = -this.mouseY + (this.mouseY = this.getMouseY(evt));
 
 		//Move panel by same amount as mouse:
 		this.windowX += diffX;
@@ -150,12 +172,10 @@ Window.prototype = {
 		var elt = this.element;
 
 		//Capture initial positions to compare to later:
-		this.mouseX = evt.clientX + (window.scrollX || document.body.scrollLeft);
-		this.windowW = elt.offsetWidth;
-		this.mouseY = evt.clientY + (window.scrollY || document.body.scrollTop);
-		this.windowH = elt.offsetHeight;
-
-		//document.body.className += " panel-dragging"; //hides controls while dragging
+		this.mouseX = this.getMouseX(evt);
+		this.windowW = this.getWidth();
+		this.mouseY = this.getMouseY(evt);
+		this.windowH = this.getHeight();
 
 		this.stopResize();
 		var thisRef = this;
@@ -167,17 +187,17 @@ Window.prototype = {
 		var elt = this.element;
 
 		//Calc distance mouse moved:
-		var diffX = -this.mouseX + (this.mouseX = evt.clientX + (window.scrollX || document.body.scrollLeft));
-		var diffY = -this.mouseY + (this.mouseY = evt.clientY + (window.scrollY || document.body.scrollTop));
+		var diffX = -this.mouseX + (this.mouseX = this.getMouseX(evt));
+		var diffY = -this.mouseY + (this.mouseY = this.getMouseY(evt));
 
 		//Move panel by same amount as mouse:
 		this.windowW += diffX;
 		this.windowH += diffY;
 
 		//Commit:
-		var minW = Window.minWidth || 100;
-		var minH = Window.minHeight || 100;
-		elt.style.width =  (this.windowW > minW ? this.windowW : minW) + "px";
+		var minW = Window.minWidth;
+		var minH = Window.minHeight;
+		elt.style.width  = (this.windowW > minW ? this.windowW : minW) + "px";
 		elt.style.height = (this.windowH > minH ? this.windowH : minH) + "px";
 	},
 	
@@ -187,5 +207,28 @@ Window.prototype = {
 		//Remove event listeners:
 		document.removeEventListener("mousemove", this.docMousemoveHandler, false);
 		document.removeEventListener("mouseup", this.docMouseupHandler, false);
+	},
+	
+	//Utilities to get properties, dimensions:
+	getLength : function(prop) {
+		return parseFloat(window.getComputedStyle(this.element,null).getPropertyValue(prop));
+	},	
+	getWidth : function() {
+		return this.element.offsetWidth || ( this.getLength("width") + 
+			this.getLength("padding-left") + this.getLength("padding-right") +
+			this.getLength("border-left-width") + this.getLength("border-right-width")		
+		);
+	},	
+	getHeight : function() {
+		return this.element.offsetHeight || ( this.getLength("height") + 
+			this.getLength("padding-top") + this.getLength("padding-bottom") +
+			this.getLength("border-top-width") + this.getLength("border-bottom-width")		
+		);
+	},
+	getMouseX : function(evt) {
+		return evt.pageX || (evt.clientX + (window.scrollX || document.body.scrollLeft));
+	},
+	getMouseY : function(evt) {
+		return evt.pageY || (evt.clientY + (window.scrollY || document.body.scrollTop));
 	}
 };
