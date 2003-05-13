6d664bb0d071883dd46ba582cf34ab39f15a9cf0
Jason Johnston <github@lojjic.net>
2003-05-13 16:51:03 +0000

    allow type to be specified for popup; creating a typed popup will destroy all other popups of that type only.

--- PopupObject.js
+++ PopupObject.js
@@ -1,7 +1,8 @@
 /*=== Base Class for Popup Widgets ===*/
 
-function PopupObject() {
+function PopupObject(type) {
 	// Freaks out Mac IE: // if(!(this instanceof PopupObject)) return new PopupObject();
+	this.popupType = type || "popup-object";
 }
 
 PopupObject.prototype = {
@@ -9,33 +10,33 @@ PopupObject.prototype = {
 	popupNode : {},
 
 	create : function(evt) {
-		if(PopupObject.currentPopup) PopupObject.currentPopup.destroy();
-		PopupObject.currentPopup = this;
+		PopupObject.destroyType(this.popupType);
+		var thisRef = PopupObject.popupsByType[this.popupType] = this;
 
 		evt.stopPropagation(); //avoid immediate destruction
 		evt.preventDefault(); //cancels link if any
-		document.addEventListener("mousedown",this.destroy,false);
-		window.addEventListener("keypress",this.keypressed,false);
+		document.addEventListener("mousedown",this.docMousedownHandler=function(){thisRef.destroy()},false);
+		window.addEventListener("keypress",this.docKeypressHandler=function(evt){thisRef.keypressed(evt)},false);
 
 		this.popupNode=document.createElement("div")
-		this.popupNode.className="popup-node"; //default class - can override in constructor
-		this.popupNode.addEventListener("mousedown",this.clicked,false);
+		this.popupNode.className=this.popupType; //default class - can override in constructor
+		this.popupNode.addEventListener("mousedown",this.pressed,false);
 		document.getElementsByTagName("body").item(0).appendChild(this.popupNode);
 	},
 
 	setPosition : function(evt,posString) {
 		if(!posString) var posString="topleft,0,0";
 		var pos=posString.split(","); //relation to cursor, x offset, y offset
-		var p = PopupObject.currentPopup;
-
+		
+		this.popupNode.style.position="absolute";
 		var wW=window.innerWidth || document.body.clientWidth;
 		var wH=window.innerHeight || document.body.clientHeight;
 		var cX=(pos[1]) ? evt.clientX + parseInt(pos[1]) : evt.clientX;
 		var cY=(pos[2]) ? evt.clientY + parseInt(pos[2]) : evt.clientY;
 		var sX=window.scrollX || document.body.scrollLeft;
 		var sY=window.scrollY || document.body.scrollTop;
-		var pW=p.getLength("width") + p.getLength("padding-left") + p.getLength("padding-right") + 4;
-		var pH=p.getLength("height") + p.getLength("padding-top") + p.getLength("padding-bottom") + 4;
+		var pW=this.getLength("width") + this.getLength("padding-left") + this.getLength("padding-right") + 4;
+		var pH=this.getLength("height") + this.getLength("padding-top") + this.getLength("padding-bottom") + 4;
 		var pX=cX; var pY=cY;
 
 		if(pos[0]=="centered") { //centered on screen
@@ -52,35 +53,40 @@ PopupObject.prototype = {
 		if(pX-sX < 2) pX=sX+2;
 		if(pY-sY < 2) pY=sY+2;
 
-		with (p.popupNode.style) {
-			position="absolute";
+		with (this.popupNode.style) {
 			left=pX+"px"; top=pY+"px";
 		}
-
+		//window.status = pX + " x " + pY;
 	},
 
 	getLength : function(prop) {
-		return parseInt(getComputedStyle(this.popupNode,null).getPropertyValue(prop));
+		return parseFloat(getComputedStyle(this.popupNode,null).getPropertyValue(prop));
 	},
 
 	keypressed : function(evt) { //destroys popup if Esc key pressed
 		if(evt.keyCode==27) {
 			evt.stopPropagation();
-			PopupObject.currentPopup.destroy();
+			this.destroy();
 		}
 	},
 
-	clicked : function(evt) { //cancel destruction when clicked
+	pressed : function(evt) { //cancel destruction when clicked
 		evt.stopPropagation();
 	},
 
 	destroy : function() {
 		var d = document;
-		var p = PopupObject.currentPopup;
-		d.getElementsByTagName("body").item(0).removeChild(p.popupNode);
-		d.removeEventListener("mousedown",p.destroy,false);
-		window.removeEventListener("keypress",p.keypressed,false);
-		PopupObject.currentPopup=null;
+		var thisRef = this;
+		d.getElementsByTagName("body").item(0).removeChild(this.popupNode);
+		PopupObject.popupsByType[this.popupType]=null;
+		setTimeout(function(){
+			d.removeEventListener("mousedown",thisRef.docMousedownHandler,false);
+			window.removeEventListener("keypress",thisRef.docKeypressHandler,false);
+		},0);
 	}
-
 };
+
+PopupObject.popupsByType = {}; //holds hash of each active popup by type
+PopupObject.destroyType = function(type) { //destroys all of given type
+	if(type && this.popupsByType[type]) this.popupsByType[type].destroy();
+}
