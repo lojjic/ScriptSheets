71b3a95fcfb4ee796886c81cf7fa4891da01c151
Jason Johnston <github@lojjic.net>
2003-08-03 03:10:07 +0000

    code size reduction

--- ScriptSheet.js
+++ ScriptSheet.js
@@ -1,64 +1,57 @@
-
 /*
-
-This class provides a way to switch between 
-"script sheets" much like switching between 
-CSS style sheets. The preferred style choice
-is persisted in a cookie.
-
-Example:
-<link rel="scriptsheet" type="text/javascript" href="OSXBar" title="Mac OS X Style" />
-<link rel="alternate scriptsheet" type="text/javascript" href="MenuBar" title="Windows Style" />
-
+**  ScriptSheet script by Jason Johnston (jj@lojjic.net)
+**  Created July 2003.  Use freely, but give me credit.
+**
+**  This class provides a way to switch between 
+**  "script sheets" much like switching between 
+**  CSS style sheets. The preferred style choice
+**  is persisted in a cookie.
 */
-
-
 
 function ScriptSheet(title, script) {
 	this.title = title;
 	this.script = window[script];
-	ScriptSheet.instances[ScriptSheet.instances.length] = this;
+	var i = ScriptSheet.instances;
+	i[i.length] = this;
 }
 ScriptSheet.prototype = {
 	enable : function() {
-		if(this.script && this.script.enableScriptSheet) this.script.enableScriptSheet();
+		var h; if(this.script && (h = this.script.enableScriptSheet)) h();
 	},
 	disable : function() {
-		if(this.script && this.script.disableScriptSheet) this.script.disableScriptSheet();
+		var h; if(this.script && (h = this.script.disableScriptSheet)) h();
 	}
 };
 ScriptSheet.instances = [];
 ScriptSheet.getPreferredStyle = function() {
 	var i, lnk, rel, ttl;
 	var lnks = document.getElementsByTagName("link");
-	
 	//look for cookie first:
-	if(typeof Cookie == "function") {
+	if(window.Cookie) {
 		ttl = new Cookie("preferredStyle").getValue();
 		if(ttl) { //make sure the cookie value is an actual sheet title:
-			for(i=0; i<lnks.length; i++) if(lnks[i].getAttribute("title") == ttl) return ttl;
+			for(i=0; (lnk=lnks[i]); i++) if(lnk.getAttribute("title") == ttl) return ttl;
 		}
 	}
-	
 	//else get first titled sheet:
-	for(i=0; i<lnks.length; i++) {
-		rel = lnks[i].getAttribute("rel");
-		ttl = lnks[i].getAttribute("title");
-		if(rel.match(/^\s*(style|script)sheet\s*$/i) && ttl) return ttl;
+	for(i=0; (lnk=lnks[i]); i++) {
+		rel = lnk.getAttribute("rel");
+		ttl = lnk.getAttribute("title");
+		if(rel.match(/\b(style|script)sheet\b/i) && ttl) return ttl;
 	}
 	return null;
 };
 ScriptSheet.switchTo = function(title) {
-	var i, lnk, rel, ttl, hrf, match;
+	var i, lnk, rel, ttl, hrf, match, c;
+	var lnks = document.getElementsByTagName("link")
 	var scripts = [];
-	for(i=0; (lnk=document.getElementsByTagName("link")[i]); i++) {
+	for(i=0; (lnk=lnks[i]); i++) {
 		rel = lnk.getAttribute("rel");
 		ttl = lnk.getAttribute("title");
 		hrf = lnk.getAttribute("href");
-		if(rel && (match=rel.match(/^\s*((alternate)\s+)?(script|style)sheet\s*$/i)) && hrf) {
-			if(match[3]=="style") { //handle stylesheets:
-				lnk.disabled = !(ttl == title || !ttl);
-			} else {
+		if(rel && (match=rel.match(/^\s*((alternat(e|ive))\s+)?(script|style)sheet\s*$/i)) && hrf) {
+			if(match[4]=="style") lnk.disabled = !(ttl == title || !ttl); //handle stylesheets
+			else {
 				hrf = hrf.substring(hrf.indexOf("#")+1);
 				if(!lnk.scriptSheet) lnk.scriptSheet = new ScriptSheet(ttl, hrf);
 				lnk.scriptSheet.disable();
@@ -68,17 +61,15 @@ ScriptSheet.switchTo = function(title) {
 	}
 	//enable all matching scripts:
 	for(i=0; i<scripts.length; i++) scripts[i].enable();
-	
 	//remember choice:
-	if(typeof Cookie == "function") {
-		var cook = new Cookie("preferredStyle");
-		cook.setLifespan(60*60*24*365); //1 year
-		cook.setValue(title);
+	if(window.Cookie) {
+		c = new Cookie("preferredStyle");
+		c.setLifespan(60*60*24*365);
+		c.setValue(title || "");
 	}
 };
 ScriptSheet.onLoad = function() {
 	var pref = ScriptSheet.getPreferredStyle();
 	ScriptSheet.switchTo(pref);
 }
-
 window.addEventListener("load",ScriptSheet.onLoad,false);
