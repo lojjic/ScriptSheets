ea86dd563a6e7b577e81d888bddabed83dd9b7b4
Jason Johnston <github@lojjic.net>
2003-08-06 16:21:39 +0000

    * Don't construct collapser/outline elements on a node until that node is shown, to improve performance in large lists * Make get/setOpenNode(s) functions of TreeMenu, to avoid code duplication

--- TreeMenu.js
+++ TreeMenu.js
@@ -3,7 +3,6 @@
 
 TODO:
 	* Make keyboard-navigable (insert <a>s around branch labels)
-	* Don't construct collapser/outline elements on a node until that node is shown, to improve performance in large lists
 
 ISSUES:
 	* If <li> IDs are different across pages, persistence will be wrong (maybe associate URIs with IDs?)
@@ -20,28 +19,39 @@ TreeMenu.prototype = {
 	create : function() {
 		var i, li;
 		this.element.className += " tree-menu";
-		this.menuNodes = [];
-		var open = {};
-		if(typeof Cookie == "function") {
-			var nodes = new Cookie("treeMenuOpenNodes").getValue();
-			if(nodes && typeof nodes == "object" && nodes.constructor == Array) {
-				for(i in nodes) open[nodes[i]] = 1; //store in hash for quick access
-			}
-		}
-		for(i=0; (li=this.element.getElementsByTagName("li")[i]); i++) {
-			this.menuNodes[this.menuNodes.length] = new TreeMenuNode(li, open[li.id]);
+		this.nodes = [];
+		for(i=0; (li=this.element.childNodes[i]); i++) {
+			if(li.nodeType==1) this.nodes[this.nodes.length] = new TreeMenuNode(li, TreeMenu.getOpenNodes()[li.id]);
 		}
 	},
-	
 	destroy : function() {
 		var i, node;
-		for(i=0; (node=this.menuNodes[i]); i++) {
-			node.destroy();
-		}
+		for(i=0; (node=this.nodes[i]); i++) node.destroy();
 		this.element.className = this.element.className.replace("tree-menu","");
 	}
 };
 TreeMenu.instances = [];
+TreeMenu.getOpenNodes = function() {
+	var open = {};
+	if(!window.Cookie) return open;
+	var c = new Cookie("treeMenuOpenNodes").getValue();
+	if(c && typeof c == "object" && c.constructor == Array) {
+		for(var i in c) open[c[i]] = 1; //store in hash for quick access
+	}
+	return open;
+};
+TreeMenu.setOpenNode = function(id, isOpen) {
+	if(!window.Cookie) return;
+	var cookie = new Cookie("treeMenuOpenNodes");
+	var oldNodes = cookie.getValue();
+	var newNodes = [];
+	if(typeof oldNodes == "object" && oldNodes.constructor == Array) {
+		for(i in oldNodes) if(oldNodes[i] != id) newNodes[newNodes.length] = oldNodes[i];
+		if(isOpen) newNodes[newNodes.length] = id;
+	}
+	cookie.setValue(newNodes);
+	cookie.setLifespan(60*60*24*365);
+};
 TreeMenu.enableScriptSheet = function() {
 	TreeMenu.disableScriptSheet();
 	var uls = document.getElementsByTagName("ul");
@@ -67,7 +77,8 @@ function TreeMenuNode(elt, open) {
 TreeMenuNode.prototype = {
 	create : function() {
 		var v, h, i, j;
-		var elt = this.element;		
+		var elt = this.element;
+		this.nodes = [];
 
 		//create dotted lines:
 		v = this.outlineVert = document.createElement("span");
@@ -119,13 +130,20 @@ TreeMenuNode.prototype = {
 	},
 	
 	expand : function() {
-		var i, c;
+		var i, j, c, li;
 		this.collapsed = false;
 		
 		//show children:
 		var kids = this.element.childNodes;
-			for(i=0; i<kids.length; i++) if(kids[i].nodeType==1 && kids[i].tagName.toLowerCase()=="ul") kids[i].style.display="";
-		
+		for(i=0; i<kids.length; i++) {
+			if(kids[i].nodeType==1 && kids[i].tagName.toLowerCase()=="ul") {
+				kids[i].style.display="";
+				for(j=0; (li=kids[i].childNodes[j]); j++) {
+					if(li.nodeType==1) this.nodes[this.nodes.length] = new TreeMenuNode(li, TreeMenu.getOpenNodes()[li.id]);
+				}
+			}
+		}
+
 		//change icon:
 		c = this.collapser;
 			c.firstChild.nodeValue = "-"; 
@@ -134,17 +152,7 @@ TreeMenuNode.prototype = {
 		
 		//persist state:
 		var id = this.element.getAttribute("id");
-		if(id && typeof Cookie == "function") {
-			var cookie = new Cookie("treeMenuOpenNodes");
-			var oldNodes = cookie.getValue();
-			var newNodes = [];
-			if(typeof oldNodes == "object" && oldNodes.constructor == Array) {
-				for(i in oldNodes) if(oldNodes[i] != id) newNodes[newNodes.length] = oldNodes[i];
-				newNodes[newNodes.length] = id;
-			}
-			cookie.setValue(newNodes);
-			cookie.setLifespan(60*60*24*365);
-		}
+		if(id) TreeMenu.setOpenNode(id, true);
 	},
 	
 	collapse : function() {
@@ -153,8 +161,9 @@ TreeMenuNode.prototype = {
 		
 		//hide children:
 		var kids = this.element.childNodes;
-			for(i=0; i<kids.length; i++) if(kids[i].nodeType==1 && kids[i].tagName.toLowerCase()=="ul") kids[i].style.display="none";
-
+		for(i=0; i<kids.length; i++) if(kids[i].nodeType==1 && kids[i].tagName.toLowerCase()=="ul") kids[i].style.display="none";
+		for(i=0; i<this.nodes.length; i++) this.nodes[i].destroy(); this.nodes = [];
+			
 		//change icon:
 		c = this.collapser;
 		c.firstChild.nodeValue = "+"; 
@@ -163,27 +172,20 @@ TreeMenuNode.prototype = {
 
 		//persist state:
 		var id = this.element.getAttribute("id");
-		if(id && typeof Cookie == "function") {
-			c = new Cookie("treeMenuOpenNodes");
-			var oldNodes = c.getValue();
-			var newNodes = [];
-			if(typeof oldNodes == "object" && oldNodes.constructor == Array) {
-				for(i in oldNodes) if(oldNodes[i] != id) newNodes[newNodes.length] = oldNodes[i];
-			}
-			c.setValue(newNodes);
-			c.setLifespan(60*60*24*365);
-		}
+		if(id) TreeMenu.setOpenNode(id, false);
 	},
 	
 	destroy : function() {
+		var i;
 		var elt = this.element;
 		elt.className = elt.className.replace(/\btree-menu-node-(branch|leaf)\b/g, "");
 		var uls = this.element.getElementsByTagName("ul");
-			for(var i=0; i<uls.length; i++) uls[i].style.display="";
+			for(i=0; i<uls.length; i++) uls[i].style.display="";
 		elt.removeChild(this.outlineVert);
 		elt.removeChild(this.outlineHoriz);
 		if(this.collapser) elt.removeChild(this.collapser);
 		if(this.icon) elt.removeChild(this.icon);
 		elt.removeEventListener("click", this.clickHandler, false);
+		for(i in this.nodes) this.nodes[i].destroy();
 	}
 }
